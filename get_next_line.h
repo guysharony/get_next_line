d64e0b225c67f08ac131ec2ec8a6aba2af0443ec
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-10-23 11:20:07 +0200

    get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -5,24 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/19 09:50:55 by gsharony          #+#    #+#             */
-/*   Updated: 2019/10/21 13:54:45 by gsharony         ###   ########.fr       */
+/*   Created: 2019/10/23 10:31:49 by gsharony          #+#    #+#             */
+/*   Updated: 2019/10/23 11:18:43 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
 # include <unistd.h>
-# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # include <fcntl.h>
-# include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
+char	*ft_strchr(const char *s, int c);
 void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strnew(size_t size);
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strcpy(char *dest, const char *src);
-char	*ft_strdup(const char *s1);
+int		get_next_line(int fd, char **line);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,66 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/19 09:50:00 by gsharony          #+#    #+#             */
-/*   Updated: 2019/10/21 13:14:31 by gsharony         ###   ########.fr       */
+/*   Created: 2019/10/23 10:31:09 by gsharony          #+#    #+#             */
+/*   Updated: 2019/10/23 11:19:20 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char		*ft_strdup(const char *s1)
+char		*ft_strchr(const char *s, int c)
 {
-	char	*a;
+	size_t	a;
 
-	if (!(a = (char*)malloc(sizeof(*a) * (ft_strlen(s1) + 1))))
-		return (NULL);
-	return (ft_strcpy(a, (char *)s1));
-}
-
-char		*ft_strcpy(char *dest, const char *src)
-{
-	unsigned int a;
-
-	a = 0;
-	while (src[a])
-	{
-		dest[a] = src[a];
-		a++;
-	}
-	dest[a] = '\0';
-	return (dest);
-}
-
-char		*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*a;
-	size_t	b;
-
-	b = 0;
-	if (!s)
-		return (NULL);
-	if (!(a = (char*)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	if (ft_strlen(s) > start)
-	{
-		while (s[start + b] && b < len)
-		{
-			a[b] = s[start + b];
-			b++;
-		}
-	}
-	a[b] = '\0';
-	return (a);
-}
-
-size_t		ft_strlen(const char *s)
-{
-	size_t	t;
-
-	t = 0;
-	while (s[t] != '\0')
-		t++;
-	return (t);
+	a = -1;
+	while (++a < ft_strlen(s) + 1)
+		if (*(s + a) == (char)c)
+			return ((char *)(s + a));
+	return (NULL);
 }
 
 void		*ft_memset(void *b, int c, size_t len)
@@ -79,4 +35,38 @@ void		*ft_memset(void *b, int c, size_t len)
 	while (d < len)
 		a[d++] = (unsigned char)c;
 	return (b);
+}
+
+char		*ft_strnew(size_t size)
+{
+	char	*s;
+
+	if (!(s = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	ft_memset(s, '\0', size + 1);
+	return (s);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	t;
+
+	t = 0;
+	while (s[t] != '\0')
+		t++;
+	return (t);
+}
+
+char		*ft_strcpy(char *dest, const char *src)
+{
+	unsigned int a;
+
+	a = 0;
+	while (src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
 }
